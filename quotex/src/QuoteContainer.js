09cb6b5996c737c3e42fbce3fefdfc7a460b741e
Eugene <eugeneayonga@gmail.com>
2022-07-30 19:27:18 +0300

    Built QuoteContainer.js, imported a child dependency into it, returned jsx using props

---
+++ quotex/src/QuoteContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Quote from "./Quote";
+
+function QuoteContainer({quotes, removeQuote, addToFavourites}) {
+    return (
+        <div className="quotes-container">
+            {quotes.map(quote => {
+                return (
+                    <Quote 
+                        key={quote.id} 
+                        quote={quote} // revert back to quotes
+                        removeQuote={removeQuote} 
+                        addToFavourites={addToFavourites} 
+                    />
+                );
+            })}
+        </div>
+    )
+}
+
+
+export default QuoteContainer
