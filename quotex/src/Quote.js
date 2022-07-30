53bba35701adb50dbfa0669d794ada313c2615c2
Eugene <eugeneayonga@gmail.com>
2022-07-30 19:00:32 +0300

    Imported state, declared state variables, built a delete function event listener in Quote.js

--- quotex/src/Quote.js
+++ quotex/src/Quote.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const quotesAPI = "http://localhost:3000/quotes";
 
 function Quote({quote, removeQuote, addToFavorites}) {
     const {title, content, quotee} = quote;
     const [isRead, setIsRead] = useState(false);
+
+    function onDeleteClick(event) {
+        event.preventDefault();
+        fetch(`${quotesAPI}/${quote.id}`, {
+            method: "DELETE",
+        });
+        removeQuote(quote);
+    }
 
 
   return (
