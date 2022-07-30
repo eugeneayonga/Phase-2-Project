4663d0f0164fcc9a56ce9c197f33827423486e5d
Eugene <eugeneayonga@gmail.com>
2022-07-30 19:33:40 +0300

    Imported useState and useEffect, initialized boilerplate code in App.js

--- quotex/src/App.js
+++ quotex/src/App.js
@@ -1,22 +1,17 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
+import QuoteContainer from "./QuoteContainer";
+
+const quotesAPI = "http://localhost:3000/quotes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
   );
